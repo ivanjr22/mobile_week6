fac9c90bf174df9aaf72cdf5a5a3f07880ac1639
= <you@example.com>
2021-03-16 21:32:08 +0700

    Edit HomePage

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -5,7 +5,8 @@ import 'package:mobile_week6/pages/item_page.dart';
 class HomePage extends StatelessWidget{
   final List<Item> Items = [ 
    Item(kode : 'A01', name: 'Sugar', price: 5000 , stok: '10' , deskripsi: 'Gula merupakan salah satu pemanis yang umum dikonsumsi masyarakat. Gula biasa digunakan sebagai pemanis di makanan maupun minuman, dalam bidang makanan, selain sebagai pemanis, gula juga digunakan sebagai stabilizer dan pengawet. Gula merupakan suatu karbohidrat sederhana yang umumnya dihasilkan dari tebu.' ),
-   Item(kode : 'B01', name: 'Salt', price: 2000, stok: '15' , deskripsi: 'Dalam ilmu kimia, garam adalah senyawa ionik yang terdiri dari ion positif (kation) dan ion negatif (anion), sehingga membentuk senyawa netral (tanpa bermuatan). Garam terbentuk dari hasil reaksi asam dan basa. Natrium klorida (NaCl), bahan utama garam dapur adalah suatu garam.' )
+   Item(kode : 'B01', name: 'Salt', price: 2000, stok: '15' , deskripsi: 'Dalam ilmu kimia, garam adalah senyawa ionik yang terdiri dari ion positif (kation) dan ion negatif (anion), sehingga membentuk senyawa netral (tanpa bermuatan). Garam terbentuk dari hasil reaksi asam dan basa. Natrium klorida (NaCl), bahan utama garam dapur adalah suatu garam.' ),
+   Item(kode : 'C01', name: 'Cooking Oil 1 LT', price: 15000, stok: '5' , deskripsi: 'Minyak masakan adalah minyak atau lemak yang berasal dari pemurnian bagian tumbuhan, hewan, atau dibuat secara sintetik yang dimurnikan dan biasanya digunakan untuk menggoreng makanan. Minyak masakan umumnya berbentuk cair dalam suhu kamar.' )
  ];
 
   @override
